b3000bff64d83486af168cc1bd07a704bb77fbbe
John Hunt <john@matraining.com>
2019-10-31 17:53:08 +0000

    Person class with a constrcutor

--- 02-classes/src/com/jjh/people/Person.scala
+++ 02-classes/src/com/jjh/people/Person.scala
@@ -1,16 +1,19 @@
+package com.jjh.people
 
 /**
  *  A person who uses our application.
  *
  * @constructor create a new person with a name and age.
- * @param name the person's name
+ * @param name the person's name (immutable)
  * @param age the person's age in years 
  */
 class Person(val name: String, var age: Int) { }
 
 object TestPersonApp extends App {
-  val p1 = new Person("John", 55)
+  val p1 = new Person(name ="John", age = 55)
   println(p1.name + " is " + p1.age)
   val p2 = new Person("Denise", 53)
   println(p2.name + " is " + p2.age)
+  val p3 = new Person("Jasmine", 20)
+  println(p3.name + " is " + p3.age)
 }
