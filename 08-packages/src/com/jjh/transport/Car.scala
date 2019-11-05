2b70fa327126ad4aa056ab92b85b506c9c6729e2
John Hunt <john@matraining.com>
2019-11-05 22:21:11 +0000

    Nested packages example

--- 08-packages/src/com/jjh/transport/Car.scala
+++ 08-packages/src/com/jjh/transport/Car.scala
@@ -1,11 +1,13 @@
 package com.jjh.transport {
 
+  // Nested package personal
   package personal {
     class Bike
   }
 
   case class Car(brand: String, model: String)
 
+  // Another nexted package group
   package group {
 
     import com.jjh.transport.personal.Bike
