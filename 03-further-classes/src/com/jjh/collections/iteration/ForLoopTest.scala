6b11cfe98caae234042cd06dbf6c80667e724194
John Hunt <john@matraining.com>
2019-11-06 22:18:28 +0000

    Added further for and loop examples

--- 03-further-classes/src/com/jjh/collections/iteration/ForLoopTest.scala
+++ 03-further-classes/src/com/jjh/collections/iteration/ForLoopTest.scala
@@ -5,12 +5,23 @@ object ForLoopTest extends App {
   // Long hand form of a for loop
   // note to is a method called on the integer
   for (i <- (0).to(10)) {
-    print(i)
+    print(s"$i, ")
   }
+  println()
 
-  println("\n----------------------")
+  println("----------------------")
 
   // Shorter more common form
-  for (i <- 0 to 10) print(i)
+  for (i <- 0 to 10) print(s"$i, ")
+  println()
+
+  println("----------------------")
+  val result1 = for (i <- 1 to 5) yield 10 * i
+  println(result1)
+
+  println("----------------------")
+  val result2 = for (n <- List("one", "two", "three")) yield n.substring(0, 2)
+  print(result2)
+
 
 }
--- 03-further-classes/src/com/jjh/collections/iteration/WhileLoopTest.scala
+++ 03-further-classes/src/com/jjh/collections/iteration/WhileLoopTest.scala
@@ -3,7 +3,8 @@ package com.jjh.collections.iteration
 object WhileLoopTest extends App {
   var i = 0
   while (i < 10) {
-    println(i)
+    print(s"$i, ")
     i += 1
   }
+  println()
 }
