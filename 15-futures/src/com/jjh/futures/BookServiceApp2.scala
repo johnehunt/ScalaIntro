cae4f353f92af47ac6d55d58b01217c5a4a86922
John Hunt <john@matraining.com>
2019-11-06 00:23:13 +0000

    Added further comments

--- 15-futures/src/com/jjh/futures/BookServiceApp2.scala
+++ 15-futures/src/com/jjh/futures/BookServiceApp2.scala
@@ -7,14 +7,18 @@ object BookServiceApp2 extends App {
   import scala.concurrent._
   import ExecutionContext.Implicits.global
 
+  // Set up the future with a recovery strategy if an exception is thrown
   val future = Future {
     BookService.getBook("123")
     // BookService.getBook("999")
   } recoverWith { case e: Exception => Future {Book("not a book", "Anon", 0.0)} }
 
+  // Allow us to specify the max wait time in seconds
   import scala.concurrent.duration._
 
   val book = Await.result(future, 2 seconds)
+
+  // print the result of awaiting
   println(s"Book: $book")
 
 }
