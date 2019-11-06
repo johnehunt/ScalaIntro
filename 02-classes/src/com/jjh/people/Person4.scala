c260f585e5b1328005e537cd1dd15a2e7a6ad5d0
John Hunt <john@matraining.com>
2019-11-06 21:03:44 +0000

    fixed typo in comment

--- 02-classes/src/com/jjh/people/Person4.scala
+++ 02-classes/src/com/jjh/people/Person4.scala
@@ -5,7 +5,7 @@ class Person4(val name: String, var age: Int) {
   // Auxiliary Constructor - provides default age
   def this(name: String) = this(name, 18)
 
-  // Provides a defualt way to convert instance to a string
+  // Provides a default way to convert instance to a string
   override def toString: String = s"Person($name, $age)"
 }
 
