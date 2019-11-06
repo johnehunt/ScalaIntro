809aeecbbd06e2a92dbf171872bfc5dd93b8f8a1
John Hunt <john@matraining.com>
2019-11-06 16:50:15 +0000

    Renamed two classes to match the order of presentation in the course

--- 02-classes/src/com/jjh/people/Person3.scala
+++ 02-classes/src/com/jjh/people/Person3.scala
@@ -5,11 +5,18 @@ class Person3(val name: String, var age: Int) {
   // Auxiliary Constructor - provides default age
   def this(name: String) = this(name, 18)
 
-  // Provides a defualt way to convert instance to a string
-  override def toString: String = s"Person($name, $age)"
+  // Validates the data being received
+  // If not met will throw a java.lang.IllegalArgumentException
+  require(age > 0)
+
+  // Runs when the object is first created
+  // Can't use toString as not set yet
+  println(s"Created: $name with age of $age")
+
 }
 
-object PersonTestApp extends App {
-  val p1 = new Person3("John", 55)
-  println(p1)
+object Person3App extends App {
+  val p4 = new Person3("John", 18)
+  // val p4 = new Person4("John", -1)
+  println(s"${p4.name} is ${p4.age}")
 }
--- 02-classes/src/com/jjh/people/Person4.scala
+++ 02-classes/src/com/jjh/people/Person4.scala
@@ -5,21 +5,11 @@ class Person4(val name: String, var age: Int) {
   // Auxiliary Constructor - provides default age
   def this(name: String) = this(name, 18)
 
-  // Validates the data being received
-  // If not met will throw a java.lang.IllegalArgumentException
-  require(age > 0)
-
-  // Runs when the object is first created
-  // Can't use toString as not set yet
-  println(s"Created: $name with age of $age")
-
   // Provides a defualt way to convert instance to a string
-  override def toString: String = s"Person4($name, $age)"
-
+  override def toString: String = s"Person($name, $age)"
 }
 
-object Person4App extends App {
-  val p4 = new Person4("John", 18)
-  // val p4 = new Person4("John", -1)
-  println(p4)
+object Person4TestApp extends App {
+  val p1 = new Person4("John", 55)
+  println(p1)
 }
