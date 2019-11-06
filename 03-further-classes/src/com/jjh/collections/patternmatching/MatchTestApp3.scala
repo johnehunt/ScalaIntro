be9f0f330147f97ef2d1c1dcd9555ed3bd251f61
John Hunt <john@matraining.com>
2019-11-06 22:30:58 +0000

    Fixed bug in describe method

--- 03-further-classes/src/com/jjh/collections/patternmatching/MatchTestApp3.scala
+++ 03-further-classes/src/com/jjh/collections/patternmatching/MatchTestApp3.scala
@@ -6,7 +6,7 @@ package com.jjh.collections.patternmatching
  */
 object MatchTestApp3 {
 
- def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
     println(describe(5))
     println(describe(true))
     println(describe("hello"))
@@ -14,14 +14,12 @@ object MatchTestApp3 {
     println(describe(42.6))
   }
 
-  def describe(x: Any): Unit = {
-    x match {
-      case 5 => "five"
-      case true => "truth"
-      case "hello" => "welcome message"
-      case Nil => "It was Nil"
-      case somethingElseVariable => s"something else: $somethingElseVariable"
-    }
+  def describe(x: Any): String = x match {
+    case 5 => "five"
+    case true => "truth"
+    case "hello" => "welcome message"
+    case Nil => "It was Nil"
+    case somethingElseVariable => s"something else: $somethingElseVariable"
   }
 
 }
