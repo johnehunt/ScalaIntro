9fbb6f3bd13c7ca521c7fb7af2ebc7bd212ee628
John Hunt <john@matraining.com>
2019-11-07 17:11:39 +0000

    Added simple constructor matching example

--- 05-inheritance/src/com/jjh/expression/SealedClassExampleApp.scala
+++ 05-inheritance/src/com/jjh/expression/SealedClassExampleApp.scala
@@ -7,7 +7,15 @@ case class Number(num: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
 case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
 
-object TypeDescriber {
+object TypeDescripter {
+  def describe(x: Expr): String = x match {
+    case Var(_) => "A Variable"
+    case Number(_) => "A Number"
+    case _ => "something else"
+  }
+}
+
+object SealedTypeDescripter {
   def describe(x: Expr): String = x match {
     case Var(name) => s"Variable $name"
     case Number(44) => "Number with 44"
@@ -17,7 +25,7 @@ object TypeDescriber {
 }
 
 object SealedClassExampleApp extends App {
-  import TypeDescriber._
+  import TypeDescripter._
 
   println(describe(Var("count")))
   println(describe(Number(44)))
