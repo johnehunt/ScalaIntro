3422825a6e539b96db99050fb422e6f93419089d
John Hunt <john@matraining.com>
2019-11-08 18:01:05 +0000

    Illustrates how to deal with multiple references to a specific package

--- 09-packages/src/Control.scala
+++ 09-packages/src/Control.scala
@@ -14,7 +14,7 @@ package family {
       val b2 = new economy.engine.Petrol2
       val b3 = new family.engine.Petrol3
       // val b4 = new engine.Petrol1
-      val b5 = new _root_.engine.Petrol1
+      val b5 = new com.jjh.auto.engine.Petrol1
     }
   }
   package engine {
