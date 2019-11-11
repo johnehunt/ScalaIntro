7362cd4cdc6c50cfb1286be642fe5f6396575c39
John Hunt <john@matraining.com>
2019-11-11 13:23:54 +0000

    Illustrated use of fucntion type and allocating a method to a function variable

---
+++ 11-functions/src/com/jjh/calc/Processor.scala
@@ -0,0 +1,13 @@
+package com.jjh.calc
+
+class Processor(val label: String) {
+  def printIt(i: Int) = println(s"$label: $i")
+}
+
+object ProcesorApp extends App {
+  val proc = new Processor(">")
+  proc.printIt(5)
+  println("-" * 20)
+  val func: Int => Unit = proc.printIt
+  func(5)
+}
