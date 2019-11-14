31fbcdf5e97e6d31032d3a2117329c85fecba7de
John Hunt <john@matraining.com>
2019-11-14 18:08:56 +0000

    Added example of ScalaMock

---
+++ 15-testing/src/test/com/jjh/payroll/PayrollTestSuite.scala
@@ -0,0 +1,25 @@
+package com.jjh.payroll
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.FunSpec
+
+/**
+ * An example of using ScalaMock with ScalaTest
+ */
+class PayrollTestSuite extends FunSpec with MockFactory {
+
+  describe("A Payroll System") {
+    describe("when presented with a person") {
+      it("should generate a string indicating the amount to pay") {
+        // Create a mock object for the Person class
+        val mockPerson = mock[Person]
+        // Indicates that the calculatePay method shoudl eb called once (the default)
+        // and that when it is called it will return 55.0
+        (mockPerson.calculatePay _).expects().once().returning(55.0)
+        val response = Payroll.generatePayslip(mockPerson)
+        assert(response == "You earned 55.0")
+      }
+    }
+  }
+
+}
