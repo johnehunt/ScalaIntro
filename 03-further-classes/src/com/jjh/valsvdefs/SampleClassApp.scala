4487591e0dd96f55404444a527f352cb0aa24040
John Hunt <john@matraining.com>
2019-11-15 10:07:37 +0000

    Added an example illustrating the difference between a val and a def (with no parenthesies)

---
+++ 03-further-classes/src/com/jjh/valsvdefs/SampleClassApp.scala
@@ -0,0 +1,18 @@
+package com.jjh.valsvdefs
+
+import java.util.Date
+
+class MyClass {
+  val someTime = new Date
+  def anotherTime = new Date
+}
+
+object SampleClassApp extends App {
+  val inst = new MyClass
+  println(s"inst.someTime ${inst.someTime}")
+  println(s"inst.anotherTime: ${inst.anotherTime}")
+  Thread.sleep(5000)
+  println("-" * 20)
+  println(s"inst.someTime ${inst.someTime}")
+  println(s"inst.anotherTime: ${inst.anotherTime}")
+}
