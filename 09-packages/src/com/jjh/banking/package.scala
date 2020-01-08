7f17a21da78cc6db717ef797f410ff6dec89ef40
John Hunt <john@matraining.com>
2020-01-08 15:39:27 +0000

    Added a package object comment

--- 09-packages/src/com/jjh/banking/package.scala
+++ 09-packages/src/com/jjh/banking/package.scala
@@ -1,5 +1,10 @@
 package com.jjh
 
+/**
+ * Defines a package object for the banking package.
+ * Contains a utility method printAccount that can be imported
+ * as part of the package or independently.
+ */
 package object banking {
   def printAccount(acc: Account): Unit = {
     import acc._
