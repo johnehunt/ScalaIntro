51e87cc88d8af6f2aa6fcb972ce0944d7032f1d5
John Hunt <john@matraining.com>
2020-01-08 15:36:05 +0000

    Tidied exception handling examples

--- 14-exceptions/src/com/jjh/functional/TrySampleApp1.scala
+++ 14-exceptions/src/com/jjh/functional/TrySampleApp1.scala
@@ -1,14 +1,23 @@
 package com.jjh.functional
 
-import scala.util.Try
-
 object TrySampleApp1 extends App {
-  val i = Try ( "123".toInt )
-  println(i)
-  println(i.isSuccess)
-  println(i.isFailure)
-  println(i.get)
+
+  import scala.util.Try
+
+  val integer = Try ( "123".toInt )
+  println(integer)
+  println(integer.isSuccess)
+  println(integer.isFailure)
+  println(integer.get)
+
   val j = Try ("Foobar".toInt )
   println(j)
   println(j.getOrElse(-1))
+
+  import scala.util.{Failure, Success}
+
+  Try ( "123".toInt ) match {
+    case Success(value) => println(value)
+    case Failure(exception) => println(exception)
+  }
 }
--- 14-exceptions/src/com/jjh/functional/TrySampleApp4.scala
+++ 14-exceptions/src/com/jjh/functional/TrySampleApp4.scala
@@ -4,11 +4,14 @@ object TrySampleApp4 extends App {
 
   import scala.util.Try
 
-  val i = Try { "4".toInt } map { n => n * n }
-  println(i)
-  val j = Try ( "s".toInt ) recover {
-    case _: NumberFormatException => 0
+  val i = Try {
+    "4".toInt
   } map { n => n * n }
+  println(i)
+
+  val j = Try("s".toInt).recover {
+    case _: NumberFormatException => 0
+  }.map(n => n * n)
   println(j)
 
 }
