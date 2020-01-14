41244cdc50fd0c604c1e353249ebf9ebd088cef9
John Hunt <john@matraining.com>
2020-01-14 20:33:53 +0000

    Minor update to companion object example

--- 03-further-classes/src/com/jjh/sessions/Session.scala
+++ 03-further-classes/src/com/jjh/sessions/Session.scala
@@ -5,15 +5,19 @@ package com.jjh.sessions
  */
 class Session(var id: Int) {
   override def toString: String = "Session[" + id + "]"
+  def printInfo(): Unit = {
+    println(Session.MAX)
+  }
 }
 
 /**
  * Its Companion (singleton) object
  */
 object Session {
+  private val MAX = 100
   private var counter = 0
   private def next(): Unit = counter = counter + 1
-  def Create: Session = { next(); new Session(counter) }
+  def create(): Session = { next(); new Session(counter) }
 }
 
 
--- 03-further-classes/src/com/jjh/sessions/SessionTestApp.scala
+++ 03-further-classes/src/com/jjh/sessions/SessionTestApp.scala
@@ -4,9 +4,9 @@ package com.jjh.sessions
  * Application to illustrate the use of the Session Object 9and Companion Class.
  */
 object SessionTestApp extends App {
-  val s1 = Session.Create
+  val s1 = Session.create()
   println(s1)
-  val s2 = Session.Create
+  val s2 = Session.create()
   println(s2)
   val s3 = new Session(42)
   println(s3)
