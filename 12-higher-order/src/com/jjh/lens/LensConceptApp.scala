53d6e49915b64f03da8844534de6ac3881339f25
John Hunt <john@matraining.com>
2020-01-15 08:44:02 +0000

    Created example to illustrate how functions can be used to create a lens like structure

---
+++ 12-higher-order/src/com/jjh/lens/LensConceptApp.scala
@@ -0,0 +1,73 @@
+package com.jjh.lens
+
+case class Street(name: String, number: Int)
+
+case class Address(country: String, city: String, street: Street)
+
+case class User(id: Int, address: Address)
+
+object Util {
+  // Non Lens style of update
+  // Can get difficult to read, convoluted, easy to get wrong
+  def changeStreetNumber(user: User, i: Int): User = {
+    user.copy(
+      address = user.address.copy(
+        street = user.address.street.copy(
+          number = i
+        )
+      )
+    )
+  }
+}
+
+object LensUtils {
+
+  // Defines a Lens used to access and update
+  // values - reusable as get and set are functions
+  case class Lens[A, B](get: A => B,
+                        set: (A, B) => A
+                       )
+
+  // Can combine Lenses together to allow deeper lenses
+  def compose[Outer, Inner, Value](
+                                    outer: Lens[Outer, Inner],
+                                    inner: Lens[Inner, Value]
+                                  ): Lens[Outer, Value] = Lens[Outer, Value](
+    get = outer.get andThen inner.get,
+    set = (obj, value) => outer.set(obj, inner.set(outer.get(obj), value))
+  )
+
+}
+
+object LensIdeaApp extends App {
+
+  import LensUtils._
+
+  // Inner most lens
+  val streetNumberLens = Lens[Street, Int](
+    get = _.number,
+    set = (a, b) => a.copy(number = b)
+  )
+
+  val street = Street("High Street", 10)
+  val address = Address("UK", "London", street)
+  val user = User(123, address)
+
+  println(streetNumberLens.get(street))
+  println(streetNumberLens.set(street, 9))
+
+  // Lens used with address to access a street
+  val addressStreetLens = Lens[Address, Street](
+    get = _.street,
+    set = (a, b) => a.copy(street = b)
+  )
+
+  // Combining the lenses together
+  val addressStreetNumberLens: Lens[Address, Int] =
+    compose(addressStreetLens, streetNumberLens)
+
+  println(addressStreetLens.get(address))
+  println(addressStreetNumberLens.set(address, 8))
+
+
+}
