085a8c492b26fd42519f72de97b16f4d4340361b
John Hunt <john@matraining.com>
2020-01-15 19:29:02 +0000

    Layout changes

--- 14-exceptions/src/com/jjh/functional/TrySampleApp3.scala
+++ 14-exceptions/src/com/jjh/functional/TrySampleApp3.scala
@@ -3,8 +3,10 @@ package com.jjh.functional
 import scala.util.Try
 
 object TrySampleApp3 extends App {
-  val i = Try { "4".toInt } map { n => n * n }
+  val i = Try ( "4".toInt ).map (n => n * n )
   println(i)
+  // Recover takes a PartialFunction that only handles
+  // Exceptions
   val j = Try ( "s".toInt ) recover {
     case nfe: NumberFormatException =>
       println(nfe)
--- 14-exceptions/src/com/jjh/functional/TrySampleApp4.scala
+++ 14-exceptions/src/com/jjh/functional/TrySampleApp4.scala
@@ -4,12 +4,14 @@ object TrySampleApp4 extends App {
 
   import scala.util.Try
 
-  val i = Try {
+  val i = Try(
     "4".toInt
-  } map { n => n * n }
+  ).map(n => n * n)
   println(i)
 
-  val j = Try("s".toInt).recover {
+  val j = Try(
+    "s".toInt
+  ).recover{
     case _: NumberFormatException => 0
   }.map(n => n * n)
   println(j)
