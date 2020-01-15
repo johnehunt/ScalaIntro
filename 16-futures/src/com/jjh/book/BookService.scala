c71c6b6a09cc7b5943fa47b83aa63fa12b19924c
John Hunt <john@matraining.com>
2020-01-15 19:47:03 +0000

    Layout modified

--- 16-futures/src/com/jjh/book/BookService.scala
+++ 16-futures/src/com/jjh/book/BookService.scala
@@ -5,7 +5,10 @@ package com.jjh.book
  */
 object BookService {
 
-  val books: Map[String, Book] = Map("123" -> Book("Scala for All", "John Smith", 12.99), "345" -> Book("Scala Futures", "Adam Davies", 11.55), "456" -> Book("Scala Play", "Jasmine Jones", 15.66))
+  val books: Map[String, Book] =
+    Map("123" -> Book("Scala for All", "John Smith", 12.99),
+        "345" -> Book("Scala Futures", "Adam Davies", 11.55),
+        "456" -> Book("Scala Play", "Jasmine Jones", 15.66))
 
   def getBook(isbn: String): Book = {
     Thread.sleep(1000) // Simulated delay
