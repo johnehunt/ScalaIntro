ae43eca776abff5b27f7866bb9fe8f0343f918cf
John Hunt <john@matraining.com>
2020-01-16 20:29:05 +0000

    Added flatmap examples

---
+++ 12-higher-order/src/com/jjh/collections/FlatMapApp.scala
@@ -0,0 +1,36 @@
+package com.jjh.collections
+
+object FlatMapApp extends App {
+
+  val data = List(
+    List(Person("John", 55), Person("Denise", 52)),
+    List(Person("Phoebe", 22), Person("Adam", 20), Person("Joselyn", 18)),
+    List(Person("Jasmine,", 20), Person("Gryff", 24))
+  )
+  println(s"data: $data")
+
+  // Apply map and then flatten the result
+  val data2 = data.flatMap(list => list.filter(p => p.age < 21))
+  println(s"data2: $data2")
+
+  // Equal to
+  val data3 = data.map(list => list.filter(p => p.age < 21))
+  println(s"data3: $data3")
+  val data4 = data3.flatten // not removal of emtpy list
+  println(s"data4: $data4")
+
+  println("-" * 25)
+
+  val myToInt: String => Option[Int] = (s: String) => {
+    import scala.util.Try
+    Try {
+      Some(s.toInt)
+    } recover { case e: Exception => None }
+  }.get
+
+  val info: List[String] = List("1", "2", "foo", "3", "hi")
+  println(info)
+  val info2: List[Int] = info.flatMap(myToInt)
+  println(info2)
+
+}
