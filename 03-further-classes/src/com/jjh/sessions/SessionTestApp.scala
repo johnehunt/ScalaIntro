1a063a3a1acc674b2662003f68262f70e7bb94a2
John Hunt <john@matraining.com>
2020-01-17 10:16:11 +0000

    Added call to instance printInfo

--- 03-further-classes/src/com/jjh/sessions/SessionTestApp.scala
+++ 03-further-classes/src/com/jjh/sessions/SessionTestApp.scala
@@ -6,6 +6,7 @@ package com.jjh.sessions
 object SessionTestApp extends App {
   val s1 = Session.create()
   println(s1)
+  s1.printInfo()
   val s2 = Session.create()
   println(s2)
   val s3 = new Session(42)
