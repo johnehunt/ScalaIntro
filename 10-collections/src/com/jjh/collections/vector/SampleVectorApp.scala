57dedb5e1237225306470a6e316e96f4d7360cf1
John Hunt <john@matraining.com>
2020-01-17 16:58:42 +0000

    Provided a Vector example

---
+++ 10-collections/src/com/jjh/collections/vector/SampleVectorApp.scala
@@ -0,0 +1,20 @@
+package com.jjh.collections.vector
+
+object SampleVectorApp extends App {
+
+  val numbers = Vector(1, 2, 3)
+  println(numbers)
+
+  val numbers2 = Vector.range(1, 4)
+  println(numbers2)
+
+  val numbers3 = numbers.prepended(9)
+  println(numbers3)
+
+  // Append to the vector
+  val numbers4 = numbers :+ 7
+  println(numbers4)
+
+  val numbers5 = numbers ++ Seq(8, 9)
+  println(numbers5)
+}
