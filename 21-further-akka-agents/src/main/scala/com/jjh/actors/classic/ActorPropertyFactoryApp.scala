69f68da4301c5a75f9736185bb80749d14c8ea6a
John Hunt <john@matraining.com>
2020-01-22 10:27:16 +0000

    Property configuration factory example added

---
+++ 21-further-akka-agents/src/main/scala/com/jjh/actors/classic/ActorPropertyFactoryApp.scala
@@ -0,0 +1,25 @@
+package com.jjh.actors.classic
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+object Calculator {
+  def props: Props = Props[Calculator]
+}
+
+class Calculator extends Actor {
+  def receive: PartialFunction[Any, Unit] = {
+    case x: Int =>
+      println("Calculator received: " + x)
+      var total = x
+      for (i <- 1 to x) total = total * i
+      println("Calculator processing completed: " + total)
+  }
+}
+
+
+object ActorPropertyFactoryApp extends App {
+  val system = ActorSystem("MyActorSystem")
+  val actor = system.actorOf(Calculator.props)
+  actor ! 4
+  println("Message sent")
+}
