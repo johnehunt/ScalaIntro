b9f9a7b3293d8d48d91fd35fd1e2ed812e2c16c3
John Hunt <jeh@midmarsh.co.uk>
2020-02-24 15:55:56 +0000

    Added examples of inner classes

---
+++ 04-further-classes/src/com/jjh/inner/Graph.scala
@@ -0,0 +1,32 @@
+package com.jjh.inner
+
+class Graph {
+  class Node {
+    var connectedNodes: List[Node] = Nil
+    def connectTo(node: Node): Unit = {
+      if (!connectedNodes.exists(node.equals)) {
+        connectedNodes = node :: connectedNodes
+      }
+    }
+  }
+  var nodes: List[Node] = Nil
+  def newNode: Node = {
+    val res = new Node
+    nodes = res :: nodes
+    res
+  }
+}
+
+object GraphApp extends App {
+  println("Starting")
+  val graph1: Graph = new Graph
+  val node1: graph1.Node = graph1.newNode
+  val node2: graph1.Node = graph1.newNode
+  node1.connectTo(node2)      // legal
+
+  // Create a second graph object
+  val graph2: Graph = new Graph
+  val node3: graph2.Node = graph2.newNode
+  // node1.connectTo(node3)      // illegal! can't connect node1 to node3 as members of different graph objects
+  println("Done")
+}
---
+++ 04-further-classes/src/com/jjh/inner/Outer.scala
@@ -0,0 +1,18 @@
+package com.jjh.inner
+
+class Outer(val age: Int) {
+
+  class Inner {
+    val x = 5
+    def printer(): Unit = {
+      println(s"Inner.x $x, outer $age")
+    }
+  }
+
+}
+
+object SampleApp extends App {
+  val myOut = new Outer(8)
+  val myIn = new myOut.Inner()
+  myIn.printer()
+}
