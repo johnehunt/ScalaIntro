3d68a233307c4e79621f1be25eed6c211c26ae12
John Hunt <jeh@midmarsh.co.uk>
2020-02-27 16:25:00 +0000

    Added an example illustrating passing parameters to an actor when instantiated

---
+++ 21-further-akka-actors/src/main/scala/com/jjh/actors/classic/ActorWithParametersApp.scala
@@ -0,0 +1,22 @@
+package com.jjh.actors.classic
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+class Greeter(val prompt: String) extends Actor {
+  def receive: PartialFunction[Any, Unit] = {
+    case "hello" => println(s"$prompt Hello World")
+    case "goodbye" => println(s"$prompt Hello World")
+    case _ => println(s"$prompt Hello Whoever")
+  }
+}
+
+object Greeter {
+  def props(args: String): Props = Props(classOf[Greeter], args)
+  def props2(args: String): Props = Props(new Greeter(args))
+}
+
+object ActorWithParametersApp extends App {
+  val system = ActorSystem("MyActorSystem")
+  val actor = system.actorOf(Greeter.props(":> "))
+  actor ! "hello"
+}
