6aa53c33e598f51b55e0a27971fd4050685ab9d7
John Hunt <john@matraining.com>
2020-03-04 09:05:53 +0000

    Added some example sof using the Seq() factory

---
+++ 12-collections/src/com/jjh/collections/sequences/SeqTest.scala
@@ -0,0 +1,14 @@
+package com.jjh.collections.sequences
+
+object SeqTest extends App {
+
+  val s1 = Seq(1, 2, 3)
+  println(s1)
+
+  val s2 = Seq("One", "two", "three", "four", "five")
+  println(s2)
+
+  val s3 = Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  println(s3)
+
+}
