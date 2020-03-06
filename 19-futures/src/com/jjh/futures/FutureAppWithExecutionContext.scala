4063cb67eba70ca5a1c44445632ecd70e051af12
John Hunt <john@matraining.com>
2020-03-06 09:30:41 +0000

    Tidying examples

--- 19-futures/src/com/jjh/futures/FutureAppWithExecutionContext.scala
+++ 19-futures/src/com/jjh/futures/FutureAppWithExecutionContext.scala
@@ -1,13 +1,12 @@
 package com.jjh.futures
 
-import scala.util.{Failure, Success}
-
 object FutureAppWithExecutionContext extends App {
 
   import java.util.concurrent.{Executors}
   import scala.concurrent.{ExecutionContext, Future}
 
-  implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+  implicit val ec: ExecutionContext =
+    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
 
   val f = Future {
     Thread.sleep(2000)
