de78cc06d3e6a4a12c8cb65716c365ed247d19e8
John Hunt <john.hunt10@gmail.com>
2020-09-28 17:52:42 +0100

    Added non case object

--- 06-case-classes/src/com/jjh/lights/LightsCaseObjectApp.scala
+++ 06-case-classes/src/com/jjh/lights/LightsCaseObjectApp.scala
@@ -7,11 +7,15 @@ case object Red
 case object Yellow
 case object Green
 
+object Amber
+
 object LightsCaseObjectApp extends App {
 
   val c1 = Red
   val c2 = Yellow
   val c3 = Red
+
+  println(Amber)
 
   println(Red, Yellow, Green)
   println(c1 equals c2)
