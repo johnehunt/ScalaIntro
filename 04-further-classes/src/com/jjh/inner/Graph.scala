9f1f1bb5298407fac02f2bfb53dfafa26d769c4f
John Hunt <john.hunt10@gmail.com>
2020-09-30 15:31:14 +0100

    Added brakets to methods

--- 04-further-classes/src/com/jjh/inner/Graph.scala
+++ 04-further-classes/src/com/jjh/inner/Graph.scala
@@ -15,8 +15,8 @@ class Graph(val label: String) {
 
   var nodes: List[Node] = Nil
 
-  def newNode: Node = {
-    val res = new Node
+  def newNode(): Node = {
+    val res = new Node()
     nodes = res :: nodes
     res
   }
@@ -25,13 +25,13 @@ class Graph(val label: String) {
 object GraphApp extends App {
   println("Starting")
   val graph1: Graph = new Graph("graph1")
-  val node1: graph1.Node = graph1.newNode
-  val node2: graph1.Node = graph1.newNode
+  val node1: graph1.Node = graph1.newNode()
+  val node2: graph1.Node = graph1.newNode()
   node1.connectTo(node2)      // legal
 
   // Create a second graph object
   val graph2: Graph = new Graph("graph2")
-  val node3: graph2.Node = graph2.newNode
+  val node3: graph2.Node = graph2.newNode()
   // node1.connectTo(node3)      // illegal! can't connect node1 to node3 as members of different graph objects
   println("Done")
 }
