64992fc85c969c1733be8ce586b31a1e352d77e9
John Hunt <john.hunt10@gmail.com>
2020-09-30 09:44:58 +0100

    Made extends app

--- 08-further-inheritance/src/com/jjh/trades/SealedClassTradeApp.scala
+++ 08-further-inheritance/src/com/jjh/trades/SealedClassTradeApp.scala
@@ -7,21 +7,19 @@ case class FxTrade(currency1: String, currency2: String) extends Trade
 case class InterestRateSwap(fixedRate: Double, floatingRate: Double) extends Trade
 case class Swaption(data: String) extends Trade
 
-object SealedTypeDescripter {
+object SealedClassTradeApp extends App {
+
+  println(describe(EquityTrade("IBM")))
+  println(describe(FxTrade("GBP", "USD")))
+  println(describe(InterestRateSwap(5.0, 3.4)))
+  println(describe(Swaption("Bermudan")))
+
   def describe(x: Trade): String = x match {
     case EquityTrade(name) => s"EquityTrade $name"
     case FxTrade(curr1, curr2) => s"FxTrade $curr1, $curr2"
     case InterestRateSwap(5.0, _) => "An InterestRateSwap for 5.0"
     case Swaption("Bermudan") => "A Bermudan Swaption"
   }
-}
 
-object SealedClassTradeApp extends App {
-  import SealedTypeDescripter._
-
-  println(describe(EquityTrade("IBM")))
-  println(describe(FxTrade("GBP", "USD")))
-  println(describe(InterestRateSwap(5.0, 3.4)))
-  println(describe(Swaption("Bermudan")))
 }
 
