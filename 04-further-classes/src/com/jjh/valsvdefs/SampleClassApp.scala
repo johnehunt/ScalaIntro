66d759822a2a20c42da414ea1a06608ee9ce71be
John Hunt <john.hunt10@gmail.com>
2020-09-30 15:31:44 +0100

    Added brakets to methods

--- 04-further-classes/src/com/jjh/valsvdefs/SampleClassApp.scala
+++ 04-further-classes/src/com/jjh/valsvdefs/SampleClassApp.scala
@@ -3,12 +3,12 @@ package com.jjh.valsvdefs
 import java.util.Date
 
 class MyClass {
-  val someTime = new Date
-  def anotherTime = new Date
+  val someTime = new Date()
+  def anotherTime = new Date()
 }
 
 object SampleClassApp extends App {
-  val inst = new MyClass
+  val inst = new MyClass()
   println(s"inst.someTime ${inst.someTime}")
   println(s"inst.anotherTime: ${inst.anotherTime}")
   Thread.sleep(5000)
