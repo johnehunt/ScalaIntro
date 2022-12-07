db8526a14b6a62e5ce30b90d41fba540e55bd6a6
John Hunt <john.hunt10@gmail.com>
2022-12-07 16:35:36 +0000

    Added a further yield example

--- 04-further-classes/src/com/jjh/flowofcontrol/ForLoopTest.scala
+++ 04-further-classes/src/com/jjh/flowofcontrol/ForLoopTest.scala
@@ -25,7 +25,9 @@ object ForLoopTest extends App {
 
   println("-" * 25)
   // For loops can have a generator, definition and a filter
-  for (i <- 0 until 10; n = i * i; if i % 2 == 0) {
+  for (i <- 0 until 10;
+       n = i * i;
+       if i % 2 == 0) {
     println(s"i $i, n $n")
   }
 
@@ -34,5 +36,12 @@ object ForLoopTest extends App {
   for (i <- 0 until 10; if i % 2 == 0) {
     println(s"i $i")
   }
+
+  println("-" * 25)
+  // can omit definition or a filter
+  val results = for (i <- 0 until 10; if i % 2 == 0)
+    yield 10 * i
+
+  println(results)
 
 }
